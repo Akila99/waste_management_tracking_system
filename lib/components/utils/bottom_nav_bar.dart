458109d14c8887e53e65afb04c67f7f236cf079b
Shawaiz1470 <mshiyam@our.ecu.edu.au>
2025-01-14 21:34:42 +0530

    new commit

--- lib/components/utils/bottom_nav_bar.dart
+++ lib/components/utils/bottom_nav_bar.dart
@@ -1,62 +1,51 @@
 import 'package:flutter/material.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  final void Function(int)? onTabTapped;
+  final void Function(int) onTabTapped;
   final int currentIndex;
 
   const MyBottomNavBar({
-    Key? key,
+    super.key,
     required this.onTabTapped,
     required this.currentIndex,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: 10,
-                offset: Offset(0, -5),
-              ),
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Colors.green,
-              elevation: 0,
-              currentIndex: currentIndex,
-              onTap: onTabTapped,
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Colors.white70,
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.gps_fixed),
-                  label: 'Tracking',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.schedule),
-                  label: 'Schedule',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.more_horiz),
-                  label: 'More',
-                ),
-              ],
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      decoration: BoxDecoration(
+        color: Colors.green.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          currentIndex: currentIndex,
+          onTap: onTabTapped,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white70,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.gps_fixed),
+              label: 'Tracking',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.schedule),
+              label: 'Schedule',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.more_horiz),
+              label: 'More',
+            ),
+          ],
         ),
       ),
     );
